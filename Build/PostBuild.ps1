6f9e87feb286890d7557bb7c45354e19bf9dfd34
r12f <r12f.code@gmail.com>
2017-08-18 22:29:45 -0700

    Fix post build script.

--- Build/PostBuild.ps1
+++ Build/PostBuild.ps1
@@ -1,5 +1,5 @@
 #
 # PostBuild.ps1
 #
-Write-Host "Stopping PowerShellToolsProcessHost.exe and vstest.executionengine.x86.exe ..."
-Get-Process | ? { ($_.ProcessName -eq 'PowerShellToolsProcessHost') -or ($_.ProcessName -eq 'vstest.executionengine.x86') } | Stop-Process -Force
+Write-Host "Stopping PowerShellToolsProcessHost.exe and vstest.executionengine*.exe ..."
+Get-Process | ? { ($_.ProcessName -eq 'PowerShellToolsProcessHost') -or ($_.ProcessName.StartsWith('vstest.executionengine')) } | Stop-Process -Force
