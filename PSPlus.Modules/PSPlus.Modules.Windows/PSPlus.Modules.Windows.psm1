35d819aa1e2b9c2ae7de5b8fb8cd659226065aae
Riff Jiang <rijiang@microsoft.com>
2017-10-17 13:31:30 -0700

    Fix import module issue.

--- PSPlus.Modules/PSPlus.Modules.Windows/PSPlus.Modules.Windows.psm1
+++ PSPlus.Modules/PSPlus.Modules.Windows/PSPlus.Modules.Windows.psm1
@@ -2,7 +2,9 @@
 # PSPlus.Modules.Windows.psm1
 #
 
-$functionRootFolder = Join-Path -Path $PSScriptRoot -ChildPath "Functions\PSPlus.Modules.Windows" -Resolve
-Get-ChildItem -Path $functionRootFolder -Filter '*.ps1' | ForEach-Object {
-    . $_.FullName | Out-Null
+$functionRootFolder = Join-Path -Path $PSScriptRoot -ChildPath "Functions\PSPlus.Modules.Windows" -Resolve -ErrorAction SilentlyContinue
+if ($functionRootFolder -ne $null) {
+    Get-ChildItem -Path $functionRootFolder -Filter '*.ps1' | ForEach-Object {
+        . $_.FullName | Out-Null
+    }
 }
